fae1667884d54d0415ba9d0f49265f945fd5b4d0
Eugenio2002-tec <102171615+Eugenio2002-tec@users.noreply.github.com>
2024-04-15 21:06:21 -0600

    Update page.js

--- lifecheck-front/app/page.js
+++ lifecheck-front/app/page.js
@@ -1,25 +1,81 @@
-import Link from "next/link";
 import Image from "next/image";
+import HeroSection from "@/components/heroSection";
+import {
+  ClipboardDocumentCheckIcon,
+  MicrophoneIcon,
+  PresentationChartBarIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/solid";
 
-export default function HeroSection() {
+export default function Home() {
   return (
-    <div className="relative w-full aspect-[10/4]">
-      <Image src={"/HeroSection.jpg"} fill className="object-cover" />
-      <div className="absolute inset-0 bg-black bg-opacity-40 flex justify-left items-left">
-        <div className="text-left text-white ">
-          <h2 className="text-4xl w-1/2 uppercase font-normal pl-5 pt-44">
-            Mejora el bienestar de tu organización
-          </h2>
-          <h3 className="text-xs pl-5 pt-3 pb-3 w-1/2">
-            Para una mejor colaboración, hay que contar con una buena calidad de
-            vida. Con una evaluación que diagnosticará y retroalimentará tus
-            áreas de crecimiento personal.
-          </h3>
-          <button className="text-white ml-24 font-semibold border-solid border-2 px-24 py-2 tracking-wide text-[10px] hover:bg-[#23A28B]">
-            Iniciar evaluación
-          </button>
+    <div>
+      <HeroSection />
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 lg:w-1/4">
+              <div className="group h-full bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-[#23A28B] transition duration 1000">
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <ClipboardDocumentCheckIcon className="h-20 w-20 text-[#23A28B] p-4 group-hover:text-white transition duration 1000" />{" "}
+                </div>
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration 1000">
+                  Ingresa tus datos
+                </h1>
+                <p className="leading-relaxed mb-3 group-hover:text-white transition duration 1000">
+                  Llena una encuesta de diferentes categorías sobre tu calidad
+                  de vida para analizar los resultados.
+                </p>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4">
+              <div className="group h-full bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-[#23A28B] transition duration 1000">
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <MicrophoneIcon className="h-20 w-20 text-[#23A28B] p-4 group-hover:text-white transition duration 1000" />{" "}
+                </div>
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration 1000">
+                  Contesta platicando
+                </h1>
+                <p className="leading-relaxed mb-3 group-hover:text-white transition duration 1000">
+                  Las encuestas están integradas con voice-to-text para simular
+                  una platica amistosa.
+                </p>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4">
+              <div className="group h-full bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-[#23A28B] transition duration 1000">
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <SparklesIcon className="h-20 w-20 text-[#23A28B] p-4 group-hover:text-white transition duration 1000" />{" "}
+                </div>
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration 1000">
+                  Revisión de IA
+                </h1>
+                <p className="leading-relaxed mb-3 group-hover:text-white transition duration 1000">
+                  Utilizando AI se analizarán tus resultados para brindar una
+                  experiencia personalizada a cada usuario.
+                </p>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4">
+              <div className="group h-full bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-[#23A28B] transition duration 1000">
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <PresentationChartBarIcon className="h-20 w-20 text-[#23A28B] p-4 group-hover:text-white transition duration 1000" />{" "}
+                </div>
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration 1000">
+                Guarda tus resultados 
+                </h1>
+                <p className="leading-relaxed mb-3 group-hover:text-white transition duration 1000">
+                  Cada encuesta que termines será guardada para futuras consultas.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
