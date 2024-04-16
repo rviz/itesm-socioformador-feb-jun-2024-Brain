23e5a0f4ea0fa1f8cc548374b49fa1637e5a75a8
Eugenio2002-tec <102171615+Eugenio2002-tec@users.noreply.github.com>
2024-04-15 21:06:55 -0600

    Update index.js

--- lifecheck-front/components/header/index.js
+++ lifecheck-front/components/header/index.js
@@ -1,27 +1,26 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { Menu } from '@headlessui/react';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect } from "react";
+import { Menu } from "@headlessui/react";
+import Link from "next/link";
 
 const menuItems = {
-  'Evaluación': {
-    href: '#',
-    subcategories: []
+  Evaluación: {
+    href: "#",
+    subcategories: [],
   },
-  'Licencias': {
-    href: '/category/125479',
-    subcategories: []
+  Licencias: {
+    href: "",
+    subcategories: [],
   },
-  'Recursos': {
-    href: '#',
-    subcategories: []
+  Recursos: {
+    href: "#",
+    subcategories: [],
   },
-  'Cuenta': {
-    href: '#',
-    subcategories: []
+  Cuenta: {
+    href: "/Cuenta",
+    subcategories: [],
   },
 };
-
 
 export default function Header() {
   const [scrolling, setScrolling] = useState(false);
@@ -36,10 +35,10 @@ export default function Header() {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -47,30 +46,37 @@ export default function Header() {
     setIsSearchVisible(!isSearchVisible); // Cambia la visibilidad del componente de búsqueda
   };
   return (
-    <div className={`${scrolling ? 'bg-[#0C1B2A]' : 'bg-white from-black/50'} fixed top-0 z-30 w-full duration-500 transition-all`}>
-      <header className={`relative w-full uppercase text-[15px] ${scrolling ? 'bg-[#F3F2F1] text-[#0C1B2A]' : 'text-white'} transition-colors duration-500`}>
-        <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
-          <Link href='/' passHref><div className='text-left text-black'>Brain</div></Link>
+    <div
+      className={`${
+        scrolling ? "bg-[#0C1B2A]" : "bg-white from-black/50"
+      } fixed top-0 z-30 w-full duration-500 transition-all`}
+    >
+      <header
+        className={`relative w-full uppercase text-[15px] ${
+          scrolling ? "bg-[#F3F2F1] text-[#0C1B2A]" : "text-white"
+        } transition-colors duration-500`}
+      >
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+          <Link href="/" passHref>
+            <div className="text-left text-black">Brain</div>
+          </Link>
 
-          <div className='items-center space-x-24 py-3 hidden uppercase md:flex'>
+          <div className="items-center space-x-24 py-3 hidden uppercase md:flex">
             {Object.keys(menuItems).map((category) => (
-              <Menu as="div" className="text-l text-black font-normal uppercase" key={category}>
-                <Menu.Button>
-                  {category}
-                </Menu.Button>
+              <Menu
+                as="div"
+                className="text-l text-black font-normal uppercase"
+                key={category}
+              >
+                <Menu.Button>{category}</Menu.Button>
               </Menu>
             ))}
           </div>
 
-          <div className='flex items-center space-x-4'>
-            <Link href={'/create'}>
-            </Link>
-            
-
+          <div className="flex items-center space-x-4">
+            <Link href={"/create"}></Link>
           </div>
         </div>
-
-
       </header>
     </div>
   );
