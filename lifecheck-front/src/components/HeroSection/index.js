55ab89099322fd6873663101fc715a3ac7f93a50
Eugenio2002-tec <102171615+Eugenio2002-tec@users.noreply.github.com>
2024-04-17 12:31:32 -0600

    Home changed

--- lifecheck-front/src/components/HeroSection/index.js
+++ lifecheck-front/src/components/HeroSection/index.js
@@ -1,23 +1,25 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <div className="relative w-full aspect-[10/4]">
       <Image src={"/HeroSection.jpg"} fill className="object-cover" />
-      <div className="absolute inset-0 bg-black bg-opacity-60 flex justify-left items-left">
-        <div className="text-left text-white">
-          <h2 className="text-4xl w-1/2 uppercase font-normal pl-5 pt-44">
+      <div className="absolute inset-0 bg-black bg-opacity-60 flex justify-center items-center">
+        <div className="text-center text-white px-4 py-8">
+          <h2 className="text-4xl uppercase font-normal max-w-2xl mx-auto pt-24">
             Mejora el bienestar de tu organización
           </h2>
-          <h3 className="text-xs pl-5 pt-3 pb-3 w-1/2">
+          <h3 className="text-xs mt-3 mb-3 max-w-md mx-auto">
             Para una mejor colaboración, hay que contar con una buena calidad de
             vida. Con una evaluación que diagnosticará y retroalimentará tus
             áreas de crecimiento personal.
           </h3>
-          <button className="text-white ml-24 font-semibold border-solid rounded-lg border-2 px-24 py-2 tracking-wide text-[10px] hover:bg-[#23A28B]">
-            Iniciar evaluación
-          </button>
+          <div className="mt-8">
+            <button className="text-white font-semibold border-solid rounded-lg border-2 px-24 py-2 tracking-wide text-[10px] hover:bg-[#23A28B]">
+              Iniciar evaluación
+            </button>
+          </div>
         </div>
       </div>
     </div>
