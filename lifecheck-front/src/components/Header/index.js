b7caae877274808f08dfc4c77aba693ffb26241a
Eugenio2002-tec <102171615+Eugenio2002-tec@users.noreply.github.com>
2024-04-18 11:55:38 -0600

    Creación página evaluación

--- lifecheck-front/src/components/Header/index.js
+++ lifecheck-front/src/components/Header/index.js
@@ -3,6 +3,19 @@ import React, { useState, useEffect } from "react";
 import { Menu } from "@headlessui/react";
 import Link from "next/link";
 
+import { Package2, Search, CircleUser } from "lucide-react";
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/src/components/ui/dropdown-menu";
+import { Sheet, SheetTrigger, SheetContent } from "@/src/components/ui/sheet";
+
 export default function Header() {
   const [scrolling, setScrolling] = useState(false);
 
@@ -23,39 +36,44 @@ export default function Header() {
   }, []);
 
   return (
-    <div className={`${scrolling ? "bg-[#0C1B2A]" : "bg-white from-black/50"} fixed top-0 z-30 w-full duration-500 transition-all`}>
-      <header className={`relative w-full uppercase text-[15px] ${scrolling ? "bg-[#F3F2F1] text-[#0C1B2A]" : "text-white"} transition-colors duration-500`}>
+    <div className={`${scrolling ? "bg-white" : "bg-white from-black/50"} fixed top-0 z-30 border w-full duration-500 transition-all`}>
+      <header className={`relative w-full uppercase text-[15px] ${scrolling ? "bg-white text-[#0C1B2A]" : "text-white"} transition-colors duration-500`}>
         <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
           <Link href="/" className="text-left text-black cursor-pointer">Brain</Link>
 
           <div className="items-center space-x-24 py-3 hidden uppercase md:flex">
-            <Menu as="div" className="text-l text-black font-normal uppercase" key="Evaluación">
+            <Menu as="div" className="text-l text-black font-medium uppercase hover:text-[#23A28B]" key="Evaluación">
               <Menu.Button as={Link} href="/evaluation">
                 Evaluación
               </Menu.Button>
             </Menu>
-            <Menu as="div" className="text-l text-black font-normal uppercase" key="Licencias">
+            <Menu as="div" className="text-l text-black font-medium uppercase hover:text-[#23A28B]" key="Licencias">
               <Menu.Button as={Link} href="/licenses">
                 Licencias
               </Menu.Button>
             </Menu>
-            <Menu as="div" className="text-l text-black font-normal uppercase" key="Recursos">
+            <Menu as="div" className="text-l text-black font-medium uppercase hover:text-[#23A28B]" key="Recursos">
               <Menu.Button as={Link} href="/resources">
                 Recursos
               </Menu.Button>
             </Menu>
-            <Menu as="div" className="text-l text-black font-normal uppercase" key="Cuenta">
-              <Menu.Button as={Link} href="/account">
-                Cuenta
-              </Menu.Button>
-            </Menu>
           </div>
-
-          <div className="flex items-center space-x-4">
-            <Link href="/create" className="cursor-pointer">
-              Link Placeholder
-            </Link>
-          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full bg-white hover:text-[#23A28B]">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </header>
     </div>
