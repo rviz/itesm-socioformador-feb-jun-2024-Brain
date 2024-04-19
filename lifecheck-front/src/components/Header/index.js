d1a81ba1dd6215e1ce85beb1d438b43fbc668cf6
Rodrigo <a00829180@tec.mx>
2024-04-18 20:12:27 -0600

    login/logout funcional

--- lifecheck-front/src/components/Header/index.js
+++ lifecheck-front/src/components/Header/index.js
@@ -1,4 +1,5 @@
 "use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import React, { useState, useEffect } from "react";
 import { Menu } from "@headlessui/react";
 import Link from "next/link";
@@ -18,6 +19,7 @@ import { Sheet, SheetTrigger, SheetContent } from "@/src/components/ui/sheet";
 
 export default function Header() {
   const [scrolling, setScrolling] = useState(false);
+  const { user } = useUser();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,23 +38,45 @@ export default function Header() {
   }, []);
 
   return (
-    <div className={`${scrolling ? "bg-white" : "bg-white from-black/50"} fixed top-0 z-30 border w-full duration-500 transition-all`}>
-      <header className={`relative w-full uppercase text-[15px] ${scrolling ? "bg-white text-[#0C1B2A]" : "text-white"} transition-colors duration-500`}>
+    <div
+      className={`${
+        scrolling ? "bg-white" : "bg-white from-black/50"
+      } fixed top-0 z-30 border w-full duration-500 transition-all`}
+    >
+      <header
+        className={`relative w-full uppercase text-[15px] ${
+          scrolling ? "bg-white text-[#0C1B2A]" : "text-white"
+        } transition-colors duration-500`}
+      >
         <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-          <Link href="/" className="text-left text-black cursor-pointer">Brain</Link>
+          <Link href="/" className="text-left text-black cursor-pointer">
+            Brain
+          </Link>
 
           <div className="items-center space-x-24 py-3 hidden uppercase md:flex">
-            <Menu as="div" className="text-l text-black font-medium uppercase hover:text-[#23A28B]" key="Evaluación">
+            <Menu
+              as="div"
+              className="text-l text-black font-medium uppercase hover:text-[#23A28B]"
+              key="Evaluación"
+            >
               <Menu.Button as={Link} href="/evaluation">
                 Evaluación
               </Menu.Button>
             </Menu>
-            <Menu as="div" className="text-l text-black font-medium uppercase hover:text-[#23A28B]" key="Licencias">
+            <Menu
+              as="div"
+              className="text-l text-black font-medium uppercase hover:text-[#23A28B]"
+              key="Licencias"
+            >
               <Menu.Button as={Link} href="/licenses">
-                Licencias
+                Empresas
               </Menu.Button>
             </Menu>
-            <Menu as="div" className="text-l text-black font-medium uppercase hover:text-[#23A28B]" key="Recursos">
+            <Menu
+              as="div"
+              className="text-l text-black font-medium uppercase hover:text-[#23A28B]"
+              key="Recursos"
+            >
               <Menu.Button as={Link} href="/resources">
                 Recursos
               </Menu.Button>
@@ -60,18 +84,30 @@ export default function Header() {
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="secondary" size="icon" className="rounded-full bg-white hover:text-[#23A28B]">
+              <Button
+                variant="secondary"
+                size="icon"
+                className="rounded-full bg-white hover:text-[#23A28B]"
+              >
                 <CircleUser className="h-5 w-5" />
                 <span className="sr-only">Toggle user menu</span>
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem>
+                {user ? (
+                  <Link href="/account">My Account</Link>
+                ) : (
+                  <a href="/api/auth/login">Login</a>
+                )}
+              </DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem>
+                <a href="/api/auth/logout">Logout</a>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
