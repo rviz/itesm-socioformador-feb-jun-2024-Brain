904a104edbc7678831dd57bb22c39f07933c4f47
Eugenio2002-tec <102171615+Eugenio2002-tec@users.noreply.github.com>
2024-04-19 15:46:41 -0600

    se agregó educación

--- lifecheck-front/src/app/education/page.js
+++ lifecheck-front/src/app/education/page.js
@@ -1,23 +1,22 @@
-import { AcademicCapIcon, HeartIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import Image from "next/image";
+import { AcademicCapIcon} from "@heroicons/react/24/solid";
 
-export default function Evaluation() {
+
+export default function HeroSection() {
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container max-w-7xl px-5 py-24 mx-auto">
-        <div className="flex flex-col space-y-4">
-          <Link href="/education" className="block focus:outline-none w-full">
-            <div className="p-4 group h-full shadow-md bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-yellow-400 transition duration-200">
-              <div className="flex justify-center items-center">
-                <AcademicCapIcon className="h-20 w-20 text-yellow-400 p-4 group-hover:text-white transition duration-200" />
+    <div className="relative w-full aspect-[10/3]">
+      <Image src={"/Educación.jpg"} fill className="object-cover" />
+      <div className="absolute inset-0 bg-black bg-opacity-80 flex justify-center items-center">
+        <div className="text-center text-white px-4 py-8">
+          <h2 className="text-6xl uppercase font-normal max-w-2xl mx-auto pt-24">
+            Educación{" "}
+          </h2>
+          <div className="flex justify-center items-center">
+                <AcademicCapIcon className="h-32 w-32 text-yellow-400 p-4 hover:text-white transition duration-200" />
               </div>
-              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration-200">
-                Educación
-              </h1>
-            </div>
-          </Link>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
