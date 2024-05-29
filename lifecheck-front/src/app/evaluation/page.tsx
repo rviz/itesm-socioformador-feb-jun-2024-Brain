bc672559a33dc2e6003d2dc64bf1915ac30d741f
GabrielRuizMiller <110938299+GabrielRuizMiller@users.noreply.github.com>
2024-05-29 11:30:58 -0600

    EvaluationCards con colores chidos

--- lifecheck-front/src/app/evaluation/page.tsx
+++ lifecheck-front/src/app/evaluation/page.tsx
@@ -1,6 +1,7 @@
 import { AcademicCapIcon, HeartIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import EvaluationCards from "@/src/components/EvaluationCards";
+import EvaluationCard from "@/src/components/EvaluationCard";
 import {getSession} from '@auth0/nextjs-auth0'
 import { redirect } from "next/navigation";
 
@@ -9,41 +10,23 @@ export default async function Evaluation() {
   if (!data?.user) {
     redirect('/api/auth/login')
   }
+
   return (
-    <div className="flex flex-col space-y-4">
-      <EvaluationCards />
-      <Link
-        href="/evaluation/education"
-        className="block focus:outline-none w-full"
-      >
-        <button>Hola</button>
-      </Link>
+    <div className="h-screen container mx-auto px-4 py-12 md:py-16 lg:py-10">
+      <p className="text-2xl pb-12 text-center underline underline-offset-8">
+        Seleccione una categoría
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8 lg:gap-8">
+      <EvaluationCard cardName="Vivienda" myHref="/tests/speech" cardStyle={"flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-[#b27429]"} iconStyle="text-[#b27429] group-hover:text-white"/>
+      <EvaluationCard cardName="Satisfacción" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-[#487fff]" iconStyle="text-[#487fff] group-hover:text-white"/>
+      <EvaluationCard cardName="Educación" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-yellow-400" iconStyle="text-yellow-400 group-hover:text-white"/>
+      <EvaluationCard cardName="Medio ambiente" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-[#29b259]" iconStyle="text-[#29b259] group-hover:text-white"/>
+      <EvaluationCard cardName="Salud" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-pink-400" iconStyle="text-pink-400 group-hover:text-white" />
+      <EvaluationCard cardName="Ingresos" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-[#ca3ab9]" iconStyle="text-[#ca3ab9] group-hover:text-white"/>
+      <EvaluationCard cardName="Seguridad" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-emerald-600" iconStyle="text-emerald-600 group-hover:text-white"/>
+      <EvaluationCard cardName="Equilibrio trabajo-vida" myHref="/tests/speech" cardStyle="flex flex-col items-center justify-center h-full p-6 md:p-8 lg:p-24 bg-white rounded-lg transition-colors group-hover:bg-pink-400" iconStyle="text-pink-400 group-hover:text-white"/>
+      </div>
+      <div className="text-[#329788] bg-emerald-600"></div>
     </div>
-    // <section className="text-gray-600 body-font">
-    //   <div className="container max-w-7xl px-5 py-24 mx-auto">
-    //     <div className="flex flex-col space-y-4">
-    //       <Link href="/education" className="block focus:outline-none w-full">
-    //         <div className="p-4 group h-full shadow-md bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-yellow-400 transition duration-200">
-    //           <div className="flex justify-center items-center">
-    //             <AcademicCapIcon className="h-20 w-20 text-yellow-400 p-4 group-hover:text-white transition duration-200" />
-    //           </div>
-    //           <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration-200">
-    //             Educación
-    //           </h1>
-    //         </div>
-    //       </Link>
-    //       <Link href="/health" className="block focus:outline-none w-full">
-    //         <div className="p-4 group h-full shadow-md bg-white bg-opacity-75 px-8 pt-16 pb-12 rounded-lg overflow-hidden text-center relative hover:bg-pink-500 transition duration-200">
-    //           <div className="flex justify-center items-center">
-    //             <HeartIcon className="h-20 w-20 text-pink-500 p-4 group-hover:text-white transition duration-200" />
-    //           </div>
-    //           <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3 group-hover:text-white transition duration-200">
-    //             Salud
-    //           </h1>
-    //         </div>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </section>
   );
 }
