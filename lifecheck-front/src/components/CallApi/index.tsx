e59240c5a6bf06733009d5c672ac4d726323cce2
GabrielRuizMiller <110938299+GabrielRuizMiller@users.noreply.github.com>
2024-06-07 22:36:36 -0600

    GenerandoResultados

--- lifecheck-front/src/components/CallApi/index.tsx
+++ lifecheck-front/src/components/CallApi/index.tsx
@@ -8,9 +8,12 @@ import { category } from '@/src/db/schema';
 interface CallAPI
 {
   paramUserID: string;
+  ProgressBarShown: (canShow : boolean) => void;
+  showPB: boolean;
+  VoidSetAnlysisGot: (myNewResults: []) => void;
 }
 
-const MyComponent: React.FC<CallAPI> = ({ paramUserID }) => {
+const MyComponent: React.FC<CallAPI> = ({ paramUserID, ProgressBarShown, showPB, VoidSetAnlysisGot }) => {
   const [categoryIDkey, setCategoryIDkey] = useState(0);
   const [response, setResponse] = useState('');
 
@@ -61,6 +64,7 @@ const MyComponent: React.FC<CallAPI> = ({ paramUserID }) => {
 
   // TODOS LOS RESULTADOS
   async function GenerarTodosLosResultados(event) {
+    ProgressBarShown(true);
     event.preventDefault();
     let categoryChangingID = 0;
     while(categoryChangingID < 8)
@@ -85,55 +89,55 @@ const MyComponent: React.FC<CallAPI> = ({ paramUserID }) => {
         }
       }
       setProgress(100);
+      try {
+        // GET DE LOS ANALISIS
+        const responseAnalysis = await fetch('/api/analysis');
+        const dataAnalysis = await responseAnalysis.json();
+        VoidSetAnlysisGot(dataAnalysis.analysis);
+      } catch (err) {}
+      ProgressBarShown(false);
+  }
+
+  const getProgressColor = () => {
+    if(progress < 100)
+      {return "bg-gradient-to-r from-[#2e5c64] to-[#52c0d3] h-full transition-all duration-500 rounded-full";}
+    else
+    {return "bg-gradient-to-r from-[#2e6458] to-[#5dd352] h-full transition-all duration-500 rounded-full";}
   }
 
 
 
   return (
     <div>
-    <div>
-    <button onClick={GenerarTodosLosResultados} className="drop-shadow-lg group hover:scale-125 duration-200">
-                      <div className="flex justify-center items-center pt-3 group bg-white bg-opacity-75 px-2 rounded-full h-full overflow-hidden relative group-hover:bg-[#57bfd9] duration-200">
-                      <p className="pb-4 pr-2 text-center font-bold group-hover:text-white transition duration 1000">Generar Resultados</p>
-                        <SparklesIcon className="pb-4 w-7 text-[#57bfd9] group-hover:text-white transition duration 1000" />
-                    </div>
-                      </button>
-
-                      {/*<div className='bg-[#2e7c9b] rounded-full drop-shadow-lg'>
-                        <p className="text-3xl text-white font-bold text-center duration-200 mt-10">{progress}/9</p>
-                      </div>
-
-                      <progress value={0.5} className='rounded-full bg-slate-800'/>*/}
-                      
-      </div>
+    
 
       
 
-
-
-      {/*<form onSubmit={SubmitResponse}>
-
-<label htmlFor="categoryIDkey">Category ID:   </label>
-        <input
-          type="number"
-          id="categoryIDkey"
-          name="categoryIDkey"
-          value={categoryIDkey}
-          onChange={(e) => setCategoryIDkey(Number(e.target.value))}
-        /><br/>
-
-      </form>*/}
+      {(showPB == false) ? (
+        <div>
+        <button onClick={GenerarTodosLosResultados} className="drop-shadow-lg group hover:scale-125 duration-200">
+                          <div className="flex justify-center items-center pt-3 group bg-white bg-opacity-75 px-2 rounded-full h-full overflow-hidden relative group-hover:bg-[#57bfd9] duration-200">
+                          <p className="pb-4 pr-2 text-center font-bold group-hover:text-white transition duration 1000">Generar Resultados</p>
+                            <SparklesIcon className="pb-4 w-7 text-[#57bfd9] group-hover:text-white transition duration 1000" />
+                        </div>
+                          </button>
+                          
+          </div>
+        )
+        : (
+          <div>
+          <p className="text-3xl text-center duration-200 mt-20">Generando Resultados . . .</p>
+          <div className="mt-10 flex flex-col items-center justify-center bg-[#e9e9e9] rounded-full drop-shadow-lg">
+          <div className="w-full h-7 rounded-full">
+            <div
+              className={getProgressColor()}
+              style={{ width: `${progress}%` }}
+            ></div>
+          </div>
+        </div>
+        </div>
+        )}
       
-
-
-      <div className="mt-10 flex flex-col items-center justify-center bg-[#e9e9e9] rounded-full drop-shadow-lg">
-      <div className="w-full h-7 rounded-full">
-        <div
-          className="bg-gradient-to-r from-[#2e5c64] to-[#52c0d3] h-full transition-all duration-500 rounded-full"
-          style={{ width: `${progress}%` }}
-        ></div>
-      </div>
-    </div>
     </div>
   );
 }
