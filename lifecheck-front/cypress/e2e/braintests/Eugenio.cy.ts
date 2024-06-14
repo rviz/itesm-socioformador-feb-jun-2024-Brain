8674cf5f993ed57cfa5a94ec9579d6411af00b8c
Eugenio2002-tec <102171615+Eugenio2002-tec@users.noreply.github.com>
2024-06-14 14:40:59 -0600

    E2E Eugenio
    E2E Eugenio

---
+++ lifecheck-front/cypress/e2e/braintests/Eugenio.cy.ts
@@ -0,0 +1,24 @@
+describe('prueba e2e rodrigo, acceder a evaluacion solamente loggeado', () => {
+  beforeEach(() => {
+    // Ignore specific uncaught exceptions from the application
+    cy.on('uncaught:exception', (err, runnable) => {
+      // Check if the error is related to NEXT_REDIRECT
+      if (err.message.includes('NEXT_REDIRECT')) {
+        return false; // Prevent Cypress from failing the test
+      }
+      // Return false to let Cypress ignore the exception
+      return false;
+    });
+  });
+
+  it('should redirect to external login page if not logged in', () => {
+    // Step 1: Visit the home page
+    cy.visit('http://localhost:3000');
+
+    // Step 2: Verify the initial content
+    cy.contains('p', 'Descubre y mejora tu calidad de vida con nuestro análisis');
+
+    // Step 3: Attempt to access the evaluation page
+    cy.get('[href="/business"]').click();
+  });
+});
